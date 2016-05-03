563cca1e3f8d4c3f8634b9e0a61ed702f43daac0
f4nky <fankm9@bfh.ch>
2016-05-04 00:04:15 +0200

    first modifications on table view

--- attendanceCheck/attendanceCheck/MasterViewController.swift
+++ attendanceCheck/attendanceCheck/MasterViewController.swift
@@ -11,9 +11,12 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
-
-
+    var members = [
+        Member(id: 1, firstName: "John", lastName: "Doe"),
+        Member(id: 2, firstName: "Max", lastName: "Mustermann"),
+        Member(id: 3, firstName: "Felix", lastName: "Muster")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -38,7 +41,7 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
+        //objects.insert(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
@@ -47,13 +50,13 @@ class MasterViewController: UITableViewController {
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+            /*if let indexPath = self.tableView.indexPathForSelectedRow {
+                let member = members[indexPath.row]
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = member
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
-            }
+            }*/
         }
     }
 
@@ -64,14 +67,14 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return members.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        let member = members[indexPath.row]
+        cell.textLabel!.text = member.lastName + " " + member.firstName
         return cell
     }
 
@@ -82,7 +85,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
+            members.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
---
+++ attendanceCheck/attendanceCheck/Member.swift
@@ -0,0 +1,16 @@
+//
+//  Member.swift
+//  attendanceCheck
+//
+//  Created by Fanky on 03.05.16.
+//  Copyright © 2016 Fanky. All rights reserved.
+//
+
+import Foundation
+
+struct Member {
+    let id:Int
+    let firstName:String
+    let lastName:String
+    //var birthDate:NSDate
+}
