8df91581173240018ce604d15fa7e009ddf37e1a
f4nky <fankm9@bfh.ch>
2016-05-23 00:54:41 +0200

    show models in admin interface

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Period
+from .models import MemberType
+from .models import EventType
+from .models import Member
+from .models import Event
+from .models import Attendance
+
+admin.site.register(Period)
+admin.site.register(MemberType)
+admin.site.register(EventType)
+admin.site.register(Member)
+admin.site.register(Event)
+admin.site.register(Attendance)
